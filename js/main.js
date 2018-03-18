24c2eb20d22334330520dca84b7e5cfa7e03ec92
Sbalieev <16039777@student.hhs.nl>
2018-03-18 19:51:00 +0100

    Toevoegen van booleans, tweenmax
    Hierbij voeg ik booleans toe die o.a. de secondes van kleur laat veranderen en booleans voor de dagen. Daarnaast ook een Tweenmax funtie toegevoegd.

--- js/main.js
+++ js/main.js
@@ -8,14 +8,30 @@ function showTijd() {
   var tijd = u + ":" + m + "<span id='sec'>" + s + "s</span>";
   document.getElementById("tijd").innerHTML = tijd;
 
+  if(u >= 0 && u <= 4){
+    document.getElementById("message").innerHTML = "Het is nacht";
+  } else if(u >= 5 && u <= 12) {
+    document.getElementById("message").innerHTML = "Het is ochtend";
+  } else if(u > 12 && u <= 18) {
+    document.getElementById("message").innerHTML = "Het is middag";
+  } else if(u > 18 && u <= 23) {
+    document.getElementById("message").innerHTML = "Het is avond";
+  }
 
+  if(s >= 0 && s <= 20) {
+    document.getElementById("sec").style= "color:#18E042";
+  } else if(s >= 20 && s <= 40) {
+    document.getElementById("sec").style= "color:#EEBE00";
+  } else if(s >=40 && s <= 60) {
+    document.getElementById("sec").style= "color:red";
+  }
 }
 showTijd(); //Zorgt ervoor dat de klok meteen te zien nadat de pagin is geladen
 setInterval(showTijd, 1000); //Zorgt voor refresh
 
 function showDatum() {
   var datum = new Date();
-  var dagen = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  var dagen = ["Zondag","Maandag","Dinsdag","Woensdag","Donderdag","Vrijdag","Zaterdag"];
   document.getElementById("dag").innerHTML = dagen[datum.getDay()];
 
   var dagMaand = datum.getDate();
@@ -26,3 +42,13 @@ function showDatum() {
   document.getElementById("datum").innerHTML = lDatum;
 }
 showDatum(); //Laat de datum zien
+
+function getNaam() {
+  var naam = document.getElementById("naam").value;
+  document.getElementById("persoon").innerHTML = naam;
+
+  document.getElementById("form").style = "display:none";
+}
+
+
+TweenLite.to(".raket", 3.5, { ease: SlowMo.ease.config(0.7, 1.5, false), x: 2000, repeat:5});
